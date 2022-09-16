241113baf9d2ed3622e6666a4dd60f79a454c4f3
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2022-09-16 18:25:33 -0400

    Add redundancy for older workspace versions

--- src/util/functions/getDecorators.ts
+++ src/util/functions/getDecorators.ts
@@ -1,5 +1,7 @@
 import ts from "typescript";
 
 export function getDecorators(node?: ts.Node) {
+	if (node?.decorators) return node.decorators;
+
 	return node && ts.canHaveDecorators(node) ? ts.getDecorators(node) : undefined;
 }
