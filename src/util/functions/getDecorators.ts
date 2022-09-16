6456acbe18756e7766314c11d9696e7725b0f8a9
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2022-09-16 18:28:45 -0400

    Fix redundancy for older versions

--- src/util/functions/getDecorators.ts
+++ src/util/functions/getDecorators.ts
@@ -1,7 +1,7 @@
 import ts from "typescript";
 
 export function getDecorators(node?: ts.Node) {
-	if (node?.decorators) return node.decorators;
+	if (ts.canHaveDecorators === undefined) return node?.decorators;
 
 	return node && ts.canHaveDecorators(node) ? ts.getDecorators(node) : undefined;
 }
