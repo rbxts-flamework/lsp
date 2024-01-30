0c04e13546b7d6542472652e589fc4501c717c5f
fireboltofdeath <24422634+Fireboltofdeath@users.noreply.github.com>
2024-01-29 20:51:15 -0500

    Workaround classes not being injectable in packages

--- src/util/functions/isInjectable.ts
+++ src/util/functions/isInjectable.ts
@@ -2,12 +2,19 @@ import type ts from "typescript";
 import { Provider } from "../../util/provider";
 import { NodeMetadata } from "../nodeMetadata";
 
+// TODO: We should read `flamework.build` to avoid having to hardcode symbols.
+const FLAMEWORK_SYMBOLS = ["Components"];
+
 /**
  * Checks whether the CompletionEntry can be injected into other classes.
  */
 export function isInjectable(provider: Provider, name: string, token?: ts.Node, data?: ts.CompletionEntryData) {
 	const symbol = findSymbol(provider, name, token, data);
 	if (symbol && symbol.valueDeclaration) {
+		if (FLAMEWORK_SYMBOLS.includes(symbol.name) && data && data.fileName?.includes("@flamework")) {
+			return true;
+		}
+
 		const metadata = new NodeMetadata(provider, symbol.valueDeclaration);
 		return metadata.isRequested("injectable");
 	}
